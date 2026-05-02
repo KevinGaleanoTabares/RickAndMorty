57526e025b071b294e7104348b720a49cb35fa35
NochesBuenas <kevindavidtabares123@gmail.com>
2026-05-01 20:03:40 -0500

    :ambulance: Se realizaron los ajustes necesarios para que el backend este completamente terminado con su explicación documentada de las líneas de código confusas.

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const express = require('express'); // 3
 const mongoose = require('mongoose');
 const User = require('./models/User');
 const bcrypt = require('bcrypt');
-const user = require('./models/User');
+// const user = require('./models/User');
+const jwt = require('jsonwebtoken');
 
 
 const app = express(); // 4
@@ -17,6 +18,21 @@ mongoose.connect(process.env.MONGO_URI) // 6
     .catch(err => console.log(err)); // 8
 
 
+    const verifyToken = (req, res, next) => { // 18
+        const token = req.headers['authorization']; // 19
+
+        if (!token) {
+            return res.status(403).json({message: "Token Requerido"});
+        }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);// 20
+        req.user = decoded; // 21
+        next (); // 22
+    }   catch (error) {
+        return res.status(401).json({message: "Token Inválido"});
+    }
+    };
 
 ///////////////////////////////////////////////////////////////////////////////////////////
 
@@ -81,9 +97,37 @@ app.post('/login', async (req, res) => {
             return res.status(400).json({message: "Contraseña incorrecta"});
         }
 
-        res.json({message: "Login exitoso!"});
+        // Aquí creo el token JWT
+        const token = jwt.sign(  // 15
+            { id: user._id, email: user.email }, // 16
+            process.env.JWT_SECRET, 
+            { expiresIn: '1h'}
+        );
+
+        res.json({message: "Login exitoso!",
+            token //
+        });
     }   catch (error){
-        res.status(500).json({error: error.message}); // 15
+        res.status(500).json({error: error.message}); // 17
+    }
+    
+    app.get('/profile', verifyToken, (req, res) => { // 23 
+    res.json( {
+        message: 'Acceso permitido',
+        user: req.user // 24
+    });
+});
+});
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+app.get('/users', async (req, res) => {
+    try {
+        const users =  await User.find().select('-password'); // 25
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ error: error.message});
     }
 });
 
@@ -91,3 +135,47 @@ app.post('/login', async (req, res) => {
 ///////////////////////////////////////////////////////////////////////////////////////////
 
 
+
+app.get('/users/:id', async (req,res) => {
+    try {
+        const user = await User.findById(req.params.id).select('-password'); // 26
+        res.json(user);
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+});
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+app.put('/users/:id', verifyToken, async (req, res) => {
+    const {name, email, password, age} = req.body
+    try {
+        const updatedUser = await User.findByIdAndUpdate( 
+            req.params.id,
+            { name, email, age }, // 27
+            { new: true }
+        );  
+
+        res.json(updatedUser);
+    }   catch (error) {
+        res.status(500).json( {error: error.message} );
+    }
+});
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+
+app.delete('/users/:id', verifyToken, async (req, res) => {
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.json({ message: "Usuario eliminado"}); 
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+        
+    }
+});
